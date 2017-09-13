9f25f368e6d6d4b3148d64d8400de54fe00d589a
idexcodenvy <userpp9ceq@some.mail>
2017-09-13 20:30:35 +0000

    commit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first_change1505244726827
+//first_change1505331128238
 /*
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
