a36e819b6c18ac26b7cb0c175399b62dbca92ecd
idexcodenvy <che@eclipse.org>
2018-01-12 21:04:15 +0000

    commit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first_change1515745468322
+//first_change1515789330201
 /*
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
